a279d3c06f3da93f9f2b3a8108931fc22c0263ef
rozgarapp <rozgarapp2026@gmail.com>
2026-09-21 14:23:34 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Refund from "./pages/Refund.js";
 import About from "./pages/About.js";
 import Contact from "./pages/Contact.js";
 import Faq from "./pages/Faq.js";
-import Navbar from "./components/Navbar.js";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer.js";
 import SupportWidget from "./components/SupportWidget";
 import MatchBot from "./components/MatchBot";
