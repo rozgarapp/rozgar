75b97afa8bd2c58df4291921605018fb9550ed42
rozgarapp <rozgarapp2026@gmail.com>
2026-09-22 13:58:20 +0400

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -885,9 +885,10 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(api)
 
 app.add_middleware(
+    app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["https://rozgar-pi.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
